a3a8646d9a0bea1b959297002f3526142d08a2d3
payam <xpayamtaebix@gmail.com>
2022-05-21 14:20:00 +0430

    n

--- src/main/java/com/example/cuphead2/SceneControllers/RankingSceneController.java
+++ src/main/java/com/example/cuphead2/SceneControllers/RankingSceneController.java
@@ -25,30 +25,6 @@ public class RankingSceneController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        startBackgroundAnimation();
-    }
-
-    public void startBackgroundAnimation() {
-        DoubleProperty xPosition = new SimpleDoubleProperty(0);
-        xPosition.addListener((observable, oldValue, newValue) -> setBackgroundPositions(pane, xPosition.get()));
-
-        Timeline timeline = new Timeline(
-                new KeyFrame(Duration.ZERO, new KeyValue(xPosition, 0)),
-                new KeyFrame(Duration.seconds(100), new KeyValue(xPosition, -15000))
-        );
-        timeline.play();
-    }
-
-    void setBackgroundPositions(Region region, double xPosition) {
-        String style = "-fx-background-position: " +
-                "left " + xPosition / 6 + "px bottom," +
-                "left " + xPosition / 5 + "px bottom," +
-                "left " + xPosition / 4 + "px bottom," +
-                "left " + xPosition / 3 + "px bottom," +
-                "left " + xPosition / 2 + "px bottom," +
-                "left " + xPosition + "px bottom;";
-
-        region.setStyle(style);
     }
 
 }
